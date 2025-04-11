87f12287859414f34e811a35a66a78408e3b1f03
zhe-hong-Lin <zhehome1021@gmail.com>
2025-04-11 14:57:26 +0800

    建置環境變數

--- server/conf_e.py
+++ server/conf_e.py
@@ -1,12 +1,14 @@
-DEBUG = True
+from decouple import config  # 確保正確導入
+
+DEBUG = config('DJANGO_DEBUG'), # 是否開啟debug模式
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
-        "NAME": "marketing_system",
-        "USER": "root",
-        # "PASSWORD": "Ru,6e.4vu4wj/3",
-        "HOST": "localhost",
-        "PORT": "3306",
+        'NAME': config('MYSQL_DATABASE'), # MySQL 資料庫的名稱
+        'USER': config('MYSQL_USER'), # 使用者名稱
+        'PASSWORD': config('MYSQL_PASSWORD'), # 密碼
+        'HOST': config('DB_HOST', default='db'), # IP 地址
+        'PORT': config('DB_PORT', default='3306'), # 埠號(mysql為 3306)
         "OPTIONS": {
             "sql_mode": "traditional",
             "charset": "utf8mb4"
